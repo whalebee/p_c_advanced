8b6c81d9f9b0177f1aff5c4646c897baba1f3db1
whalebee <islhoeve@gmail.com>
2023-06-29 08:29:04 +0900

    06-29 아침

--- p_c_advanced/연습용.c
+++ p_c_advanced/연습용.c
@@ -2,29 +2,14 @@
 // #include "header.h"
 #include <stdio.h>
 
-void rotate(int* n1, int* n2, int* n3);
+
 
 int main()
 {
-	// 선언
-	int num1 = 1, num2 = 2, num3 = 3;
-	char enter = '\n';
+	/*
 
-	// 처리
-	while (enter == '\n')
-	{
-		printf("%d:%d:%d\n", num1, num2, num3);
-		scanf_s("%c", &enter, (unsigned int)sizeof(enter));
-		rotate(&num1, &num2, &num3);
-	}
+	*/
+
 
 	return 0;
-}
-
-void rotate(int* n1, int* n2, int* n3)
-{
-	int temp = *n1;
-	*n1 = *n2;
-	*n2 = *n3;
-	*n3 = temp;
 }
