c1fcda9515bcc432751c65ae4fecde950e6651a9
whalebee <islhoeve@gmail.com>
2023-06-28 15:03:32 +0900

    06-28 6교시

--- p_06_27_배열/배열.c
+++ p_06_27_배열/배열.c
@@ -313,7 +313,43 @@ int main()
 
 
 
-#pragma region
+#pragma region 포인터 기본
+// start
+// #include "header.h"
+#include <stdio.h>
+
+
+
+int main()
+{
+	/*
+	num1.100으로 선동초
+	num2.100으로 선동초
+	정수형 포인터변수 pnum선언
+
+	pnum을 num1의 주소로 초기화
+	pnum을 활용해서 num1의 값 30 증가 << 30으로 만들라는게 아니라
+
+	pnum을 num2의 주소로 초기화
+	pnum을 활용해서 num1의 값 30 감소 << 30으로 만들라는게 아니라
+
+	num1, num2 출력
+	*/
+
+	int num1 = 100, num2 = 100;
+	int* pnum;
+
+	pnum = &num1;
+	*pnum += 30;
+
+	pnum = &num2;
+	*pnum -= 30;
+
+	printf("num1: %d, num2: %d \n", num1, num2);
+
+	return 0;
+}
+
 
 
 
--- p_c_advanced/연습용.c
+++ p_c_advanced/연습용.c
@@ -6,30 +6,32 @@
 
 int main()
 {
-	/*
-	num1.100으로 선동초
-	num2.100으로 선동초
-	정수형 포인터변수 pnum선언
-
-	pnum을 num1의 주소로 초기화
-	pnum을 활용해서 num1의 값 30 증가 << 30으로 만들라는게 아니라
-
-	pnum을 num2의 주소로 초기화
-	pnum을 활용해서 num1의 값 30 감소 << 30으로 만들라는게 아니라
-
-	num1, num2 출력
+	/* 연습문제 2번
+	1. int형 변수 num1과 num2를 선언과 동시에 각각 10과 20으로 초기화
+	2. int형 포인터 변수 ptr1과 ptr2를 선언하여 각각 num1과 num2를 가리키게하자
+	3. ptr1과 ptr2를 이용해서 num1의 값을 10 증가 시키고 num2의 값을 10 감소시키자
+	4. 두 포인터 변수 ptr1과 ptr2가 가리키는 대상을 서로 바꾸자 (조건. &num1 &num2 금지 )
+	5. 마지막으로 pt1과 pt2가 가리키는 변수에 저장된 값을 출력하자.	
 	*/
 
-	int num1 = 100, num2 = 100;
-	int* pnum;
+	// 1 ~ 2
+	int num1 = 10, num2 = 20;
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* temp;
 
-	pnum = &num1;
-	*pnum += 30;
+	// 3
+	*ptr1 += 10; // num1 == 20
+	*ptr2 -= 10; // num2 == 10
+
+	// 4
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
 	
-	pnum = &num2;
-	*pnum -= 30;
+	// 5
+	printf("ptr1: %d \nptr2: %d ", *ptr1, *ptr2);
 
-	printf("num1: %d, num2: %d \n", num1, num2 );
 
 	return 0;
 }
