8695e5494d99d3fe5b113ff3af8a137eae891352
Whalebee <127908829+Whalebee@users.noreply.github.com>
2023-06-28 21:59:09 +0900

    06-28 복습

--- p_c_advanced/연습용.c
+++ p_c_advanced/연습용.c
@@ -2,37 +2,29 @@
 // #include "header.h"
 #include <stdio.h>
 
-void rotate(char* en, int* n1, int* n2, int* n3);
+void rotate(int* n1, int* n2, int* n3);
 
 int main()
 {
-	/*
-	main함수는 정수 1,2,3을 차례대로 출력한다.
-	엔터가 입력시 roate함수를 호출하고 그 결과를 받아서 출력
-	단, 엔터 이외의 값이 들어오면 프로그램을 종료 !
-	rotate함수는 아래와 같이 1:2:3을 차례대로 섞어주는 함수이다.
-	*/
+	// 선언
 	int num1 = 1, num2 = 2, num3 = 3;
 	char enter = '\n';
-	
-	printf("%d:%d:%d\n", num1, num2, num3);
 
+	// 처리
 	while (enter == '\n')
 	{
+		printf("%d:%d:%d\n", num1, num2, num3);
 		scanf_s("%c", &enter, (unsigned int)sizeof(enter));
-		rotate(&enter, &num1, &num2, &num3);
-		printf("%d:%d:%d", num1, num2, num3);
+		rotate(&num1, &num2, &num3);
 	}
 
 	return 0;
 }
 
-void rotate(char* en, int* n1, int* n2, int* n3)
+void rotate(int* n1, int* n2, int* n3)
 {
 	int temp = *n1;
 	*n1 = *n2;
 	*n2 = *n3;
 	*n3 = temp;
 }
-
-
