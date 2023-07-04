b28b8a55d3795667b302f1b88605e6a482f33c9f
whalebee <islhoeve@gmail.com>
2023-07-04 17:28:38 +0900

    07-04 마지막교시 끝

--- p_07_04_2차원배열의 활용/DDA와 DP, 구조체.c
+++ p_07_04_2차원배열의 활용/DDA와 DP, 구조체.c
@@ -312,7 +312,42 @@ int main()
 #pragma endregion
 
 
-#pragma region
+#pragma region 연습문제 -> 직원 구조체
+// start
+// #include "header.h"
+#include <stdio.h>
+
+struct employee
+{
+	char name[50];
+	char addr_num[15];
+	int  salary;
+};
+
+
+int main()
+{
+	/* 연습문제
+	문자열 형태의 종업원 이름과 주민등록번호
+	그리고 정수 형태의 급여정보를 저장할 수 있는 employee라는 이름의 구조체 정의
+
+	구조체 변수를 하나 선언한 다음 사용자 입력받아서 변수를 채우고,
+	구조체 변수에 채워진 데이터를 출력한다.
+	*/
+	struct employee salary_info;
+
+	fputs("이름은?: ", stdout);
+	scanf_s("%s", salary_info.name, (unsigned int)sizeof(salary_info.name));
+	fputs("주민등록번호는?: ", stdout);
+	scanf_s("%s", salary_info.addr_num, (unsigned int)sizeof(salary_info.addr_num));
+	fputs("월급은?: ", stdout);
+	scanf_s("%d", &salary_info.salary);
+
+
+	printf("--- 종합 정보--- \n\n이름: %s \n주민등록번호: %s \n월급은?: %d", salary_info.name, salary_info.addr_num, salary_info.salary);
+
+	return 0;
+}
 
 
 
--- p_c_advanced/연습용.c
+++ p_c_advanced/연습용.c
@@ -1,61 +1,35 @@
 // start
 // #include "header.h"
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 
-struct person {
+struct employee
+{
 	char name[50];
-	char phoneNum[50];
-	int age;
+	char addr_num[15];
+	int  salary;
 };
+
 
 int main()
 {
-	/* 예제 
-	person형 구조체 정의
-	- 이름.name
-	- 전화번호.phoneNum
-	- 나이.age
+	/* 연습문제
+	문자열 형태의 종업원 이름과 주민등록번호
+	그리고 정수 형태의 급여정보를 저장할 수 있는 employee라는 이름의 구조체 정의
 
-	main함수에서
-	- person형 변수 man1, man2 선언
-	- 이름에 김찬호 -> strcpy() 사용 -> 소속은 string.h
-	- 010-123-4567
-	- 나이에 40
-
-	이름, 번호, 나이 출력
-
-	(man2)
-	사용자로부터 이름, 번호, 나이 입력 받기
-	이름, 번호, 나이 출력
+	구조체 변수를 하나 선언한 다음 사용자 입력받아서 변수를 채우고,
+	구조체 변수에 채워진 데이터를 출력한다.
 	*/
-	
-	// 선언
-	struct person man1;
-	struct person man2;
+	struct employee salary_info;
 
-	// 입력
-	strcpy_s(man1.name, sizeof(man1.name), "김찬호");
-	strcpy_s(man1.phoneNum, sizeof(man1.phoneNum), "010-123-4567");
-	man1.age = 40;
+	fputs("이름은?: ", stdout);
+	scanf_s("%s", salary_info.name, (unsigned int)sizeof(salary_info.name));
+	fputs("주민등록번호는?: ", stdout);
+	scanf_s("%s", salary_info.addr_num, (unsigned int)sizeof(salary_info.addr_num));
+	fputs("월급은?: ", stdout);
+	scanf_s("%d", &salary_info.salary);
 
-	fputs("이름? ", stdout);
-	scanf_s("%s", man2.name, (unsigned char)sizeof(man2.name));
-	fputs("번호? ", stdout);
-	scanf_s("%s", man2.phoneNum, (unsigned char)sizeof(man2.phoneNum));
-	fputs("나이? ", stdout);
-	scanf_s("%d", &man2.age);
 
-	
-	// 출력
-	puts(" ");
-	printf("입력되어있던 사용자\n");
-	printf("이름: %s\n번호: %s\n나이: %d\n", man1.name, man1.phoneNum, man1.age);
-	
-	puts(" ");
-	printf("입력받은 사용자 \n");
-	printf("이름: %s\n번호: %s\n나이: %d\n", man2.name, man2.phoneNum, man2.age);
+	printf("--- 종합 정보--- \n\n이름: %s \n주민등록번호: %s \n월급은?: %d", salary_info.name, salary_info.addr_num, salary_info.salary);
 
 	return 0;
 }
