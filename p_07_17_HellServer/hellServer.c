8ccd8ade5138ddbc53155d9fe80ccf45f3b6417b
whalebee <islhoeve@gmail.com>
2023-07-17 17:39:15 +0900

    07-17 왜안되지ㅋㅋ

---
+++ p_07_17_HellServer/hellServer.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+void errorHandling(char* message);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+
+	int szClntAddr;
+	char message[] = "Hell World!";
+
+	if (argc != 2) {
+		printf("Usage : %s <port> \n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		errorHandling("WSAStartup() error");
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+
+	if (hServSock == INVALID_SOCKET)
+		errorHandling("socket() error");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		errorHandling("bind() error");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+		errorHandling("listen() error");
+
+	szClntAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+	if (hClntSock == INVALID_SOCKET)
+		errorHandling("accept() error");
+
+	send(hClntSock, message, sizeof(message), 0);
+
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	WSACleanup();
+
+	return 0;
+}
+
+void errorHandling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
